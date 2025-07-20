9565003b66ba0fdb544a8b2f5860e7b0023d0a2f
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2025-07-20 14:25:14 +0530

    minor fix

--- fileShare/fileShare/settings.py
+++ fileShare/fileShare/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bhardwajvaibhav2412@gmail.com'  # your Gmail address
-EMAIL_HOST_PASSWORD = "xpot qsla bhua valx" # App Password, NOT your Gmail password
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
